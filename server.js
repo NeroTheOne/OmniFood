ba610e6017392b1496c784e8780e753433ad949c
Janmarc <janmarc.peconcillo@gmail.com>
2018-02-26 00:58:17 -0800

    used path to fix css

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const port = process.env.PORT || 3000;
 
 const hps = require('hbs');
+const path = require('path');
 const express = require('express');
 const app = express();
-
+const publicPath = path.join(__dirname, '../views');
 app.set('view engine', 'hbs');
 hps.registerPartials(__dirname + 'views/partials');
+
 // Helpers
 // hps.registerHelper()
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render(publicPath);
 });
 
 app.listen(port, () => {
